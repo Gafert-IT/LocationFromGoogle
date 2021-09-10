3bee6c26ed0dc75c4ac6ae6d37611569247205c0
Mike Gafert <mike@gafert.it>
2021-09-10 14:23:07 +0200

    IObservableObject added

---
+++ LocationFromGoogle/Core/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LocationFromGoogle.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertychanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
