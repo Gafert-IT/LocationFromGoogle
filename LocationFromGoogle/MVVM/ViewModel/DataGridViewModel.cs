f9c4cef0736759d8da7c4cd14bffc69d7d0b1d14
Mike Gafert <mike@gafert.it>
2021-09-15 16:02:43 +0200

    DatePicker, SelcetButton and Reset Button Added, Data queries added

--- LocationFromGoogle/MVVM/ViewModel/DataGridViewModel.cs
+++ LocationFromGoogle/MVVM/ViewModel/DataGridViewModel.cs
@@ -5,26 +5,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using LocationFromGoogle.MVVM.View;
 
 namespace LocationFromGoogle.MVVM.ViewModel
 {
     class DataGridViewModel : ObservableObject
     {
         private IEnumerable<TimelineObject> _TLO_List;
+        private IEnumerable<TimelineObject> JsonData;
 
         public IEnumerable<TimelineObject> TLO_List
         {
             get { return _TLO_List; }
             set
             {
-                OnPropertyChanged();
                 _TLO_List = value;
+                OnPropertyChanged();
             }
         }
         public DataGridViewModel()
         {
-            var JsonData = DeSerializer.GetDataListFromJsonFile();
-            TLO_List = JsonData;
+            JsonData = DeSerializer.GetDataListFromJsonFile();
+
+            var query = JsonData.Where((x) => x.PlaceVisit != null
+                                           && x.ActivitySegment != null)                                           
+                                .Select((x) => x);
+
+            TLO_List = query.ToList();
+        }
+
+        // DatePicker
+        private DateTime _selectedDateFrom = new DateTime(2016, 08, 01);
+        public DateTime SelectedDateFrom
+        {
+            get { return _selectedDateFrom; }
+            set 
+            {                
+                _selectedDateFrom = value;
+                OnPropertyChanged();
+            }
+        }
+        private DateTime _selectedDateUntil = new DateTime(2016,08,31);
+        public DateTime SelectedDateUntil
+        {
+            get { return _selectedDateUntil; }
+            set
+            {
+                _selectedDateUntil = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // Button Commands
+        public ICommand BTN_SelectDate => new RelayCommand(PerformSelect);
+        private void PerformSelect(object commandParameter)
+        {     
+            var query = JsonData.Where((x) => x.PlaceVisit != null 
+                                           && x.ActivitySegment != null
+                                           && x.PlaceVisit.Duration.StartTimestampDT > SelectedDateFrom
+                                           && x.PlaceVisit.Duration.StartTimestampDT < SelectedDateUntil)
+                                .Select((x) => x);
+            TLO_List = query.ToList();
+        }
+
+        public ICommand BTN_Reset => new RelayCommand(PerformReset);
+        private void PerformReset(object commandParameter)
+        {
+            var query = JsonData.Where((x) => x.PlaceVisit != null
+                                           && x.ActivitySegment != null)
+                                .Select((x) => x);
+
+            TLO_List = query.ToList();
         }
     }
 }
